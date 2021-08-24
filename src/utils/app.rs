3bbf480d8bdece177085d8c06b1be8aae502571f
Xithrius <xithrius@gmail.com>
2021-08-23 20:27:01 -0700

    A little bit of formatting.

--- src/utils/app.rs
+++ src/utils/app.rs
@@ -1,9 +1,7 @@
-use chrono::offset::Local;
-
 pub struct App {
-    /// Current value of the input box
+    /// Current value of the input box.
     pub input: String,
-    /// History of recorded messages (time, username, message)
+    /// History of recorded messages (time, username, message).
     pub messages: Vec<Vec<String>>,
 }
 
@@ -13,15 +11,5 @@ impl Default for App {
             input: String::new(),
             messages: Vec::new(),
         }
-    }
-}
-
-impl App {
-    pub fn insert_message(&mut self, user: String, message: String) {
-        self.messages.push(vec![
-            format!("{}", Local::now().format("%a %b %e %T %Y")),
-            user,
-            message,
-        ]);
     }
 }
