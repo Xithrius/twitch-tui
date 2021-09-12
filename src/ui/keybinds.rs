7df81bc8725e0d2829c9d3043e99b620367913b6
Xithrius <xithrius@gmail.com>
2021-09-11 17:22:18 -0700

    `vector2_col_sums` to `vector2_col_max`
    The original intention was never to sum up all the lengths, I just
    forgot what I was supposed to implement I guess.

--- src/ui/keybinds.rs
+++ src/ui/keybinds.rs
@@ -8,7 +8,7 @@ use tui::{
 
 use crate::{
     handlers::config::CompleteConfig,
-    utils::{colors::WindowStyles, text::vector2_col_sums},
+    utils::{colors::WindowStyles, text::vector2_col_max},
 };
 
 pub fn draw_keybinds_ui<T>(frame: &mut Frame<T>, config: CompleteConfig) -> Result<()>
@@ -17,28 +17,25 @@ where
 {
     let vertical_chunks = Layout::default()
         .direction(Direction::Vertical)
-        .margin(10)
-        .constraints([Constraint::Min(1)].as_ref())
+        .margin(5)
+        .constraints([Constraint::Percentage(100)].as_ref())
         .split(frame.size());
 
     let mut keybinds = vec![
         vec!["Description", "Keybind"],
         vec!["Bring up the chat window", config.keybinds.chat.as_str()],
-        vec![
-            "Bring up the table for keybinds",
-            config.keybinds.keybinds.as_str(),
-        ],
+        vec!["Keybinds help", config.keybinds.keybinds.as_str()],
         vec!["See all the users in chat", config.keybinds.users.as_str()],
         vec!["Quit this application", config.keybinds.quit.as_str()],
     ];
 
-    let (maximum_description_width, maximum_keybind_width) = vector2_col_sums(keybinds.clone());
+    let (maximum_description_width, maximum_keybind_width) = vector2_col_max(keybinds.clone());
 
     let column_names = keybinds.remove(0);
 
     let table_widths = vec![
-        Constraint::Length(maximum_description_width.clone()),
-        Constraint::Length(maximum_keybind_width.clone()),
+        Constraint::Min(maximum_description_width.clone()),
+        Constraint::Min(maximum_keybind_width.clone()),
     ];
 
     let table = Table::new(
@@ -47,15 +44,11 @@ where
             .map(|k| Row::new(k.clone()))
             .collect::<Vec<Row>>(),
     )
+    .header(Row::new(column_names.clone()).style(WindowStyles::new(WindowStyles::ColumnTitle)))
     .block(Block::default().borders(Borders::ALL).title("[ Keybinds ]"))
-    .column_spacing(1)
-    .style(WindowStyles::new(WindowStyles::BoarderName))
-    .header(
-        Row::new(column_names.clone())
-            .style(WindowStyles::new(WindowStyles::ColumnTitle))
-            .bottom_margin(0),
-    )
-    .widths(&table_widths);
+    .widths(&table_widths)
+    .column_spacing(2)
+    .style(WindowStyles::new(WindowStyles::BoarderName));
 
     frame.render_widget(table, vertical_chunks[0]);
 
