ccffc550994dd57a3ffb3dfc8c2fda0d60c4ed24
Xithrius <xithrius@gmail.com>
2021-10-12 12:10:35 -0700

    Moved MacOS and Linux path testing to same test.
    Triggers based off of `any` statement, between "macos" and "linux".
    
    Pathing to config file was also changed to be dynamically changed given
    the `CARGO_BIN_NAME` environment variable.

--- src/utils/pathing.rs
+++ src/utils/pathing.rs
@@ -1,13 +1,15 @@
 use std::panic::panic_any;
 
+const BINARY_NAME: &str = env!("CARGO_BIN_NAME");
+
 pub fn config_path() -> String {
     match std::env::consts::OS {
         "linux" | "macos" => match std::env::var("HOME") {
-            Ok(env_home_path) => format!("{}/.config/ttc/config.toml", env_home_path),
+            Ok(env_home_path) => format!("{}/.config/{}/config.toml", env_home_path, BINARY_NAME),
             Err(err) => panic_any(err),
         },
         "windows" => match std::env::var("APPDATA") {
-            Ok(appdata_path) => format!("{}\\{}", appdata_path, "ttc\\config.toml"),
+            Ok(appdata_path) => format!("{}\\{}\\config.toml", appdata_path, BINARY_NAME),
             Err(err) => std::panic::panic_any(err),
         },
         _ => unimplemented!(),
@@ -24,31 +26,19 @@ mod tests {
         match std::env::var("APPDATA") {
             Ok(appdata_path) => assert_eq!(
                 config_path(),
-                format!("{}\\{}", appdata_path, "ttc\\config.toml")
+                format!("{}\\{}\\config.toml", appdata_path, BINARY_NAME)
             ),
             Err(err) => std::panic::panic_any(err),
         }
     }
 
     #[test]
-    #[cfg(target_os = "macos")]
-    fn test_macos_config_path() {
+    #[cfg(any(target_os = "macos", target_os = "linux"))]
+    fn test_unix_config_path() {
         match std::env::var("HOME") {
             Ok(env_home_path) => assert_eq!(
                 config_path(),
-                format!("{}/{}", env_home_path, ".config/ttc/config.toml")
-            ),
-            Err(err) => std::panic::panic_any(err),
-        }
-    }
-
-    #[test]
-    #[cfg(target_os = "linux")]
-    fn test_linux_config_path() {
-        match std::env::var("HOME") {
-            Ok(env_home_path) => assert_eq!(
-                config_path(),
-                format!("{}/{}", env_home_path, ".config/ttc/config.toml")
+                format!("{}/.config/{}/config.toml", env_home_path, BINARY_NAME)
             ),
             Err(err) => std::panic::panic_any(err),
         }
