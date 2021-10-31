43705f48db8368fa48c3ce0e95dbb6878cfc6b7e
Xithrius <xithrius@gmail.com>
2021-10-30 22:43:47 -0700

    Added hashmap to handle multiple lines of input at a time

--- src/handlers/app.rs
+++ src/handlers/app.rs
@@ -1,5 +1,6 @@
-use std::collections::VecDeque;
+use std::collections::{HashMap, VecDeque};
 
+use maplit::hashmap;
 use rustyline::line_buffer::LineBuffer;
 use tui::layout::Constraint;
 
@@ -16,8 +17,8 @@ pub struct App {
     pub messages: VecDeque<Data>,
     /// Which window the terminal is currently showing
     pub state: State,
-    /// Current value of the input box
-    pub input_text: LineBuffer,
+    /// Current value of different input boxes
+    pub input_boxes: HashMap<&'static str, LineBuffer>,
     /// The constraints that are set on the table
     pub table_constraints: Option<Vec<Constraint>>,
     /// The titles of the columns within the table of the terminal
@@ -29,7 +30,9 @@ impl App {
         App {
             messages: VecDeque::with_capacity(data_limit),
             state: State::Normal,
-            input_text: LineBuffer::with_capacity(4096),
+            input_boxes: hashmap! {
+                "chat" => LineBuffer::with_capacity(4096),
+            },
             table_constraints: None,
             column_titles: None,
         }
