7865cee8483df8c8a1af0b0751124615ad86a7a6
Xithrius <xithrius@gmail.com>
2022-01-16 15:00:07 -0800

    File argument now accepted in config_path func

--- src/handlers/config.rs
+++ src/handlers/config.rs
@@ -65,14 +65,14 @@ pub struct FrontendConfig {
 
 impl CompleteConfig {
     pub fn new() -> Result<Self, Error> {
-        if let Ok(config_contents) = std::fs::read_to_string(config_path()) {
+        if let Ok(config_contents) = std::fs::read_to_string(config_path("config.toml")) {
             let config: CompleteConfig = toml::from_str(config_contents.as_str()).unwrap();
 
             Ok(config)
         } else {
             bail!(
                 "Configuration not found. Create a config file at '{}', and see '{}' for an example configuration.",
-                config_path(),
+                config_path("config.toml"),
                 format!("{}/blob/main/default-config.toml", env!("CARGO_PKG_REPOSITORY"))
             )
         }
--- src/utils/pathing.rs
+++ src/utils/pathing.rs
@@ -2,14 +2,14 @@ use std::panic::panic_any;
 
 const BINARY_NAME: &str = env!("CARGO_BIN_NAME");
 
-pub fn config_path() -> String {
+pub fn config_path(file: &str) -> String {
     match std::env::consts::OS {
         "linux" | "macos" => match std::env::var("HOME") {
-            Ok(env_home_path) => format!("{}/.config/{}/config.toml", env_home_path, BINARY_NAME),
+            Ok(env_home_path) => format!("{}/.config/{}/{}", env_home_path, BINARY_NAME, file),
             Err(err) => panic_any(err),
         },
         "windows" => match std::env::var("APPDATA") {
-            Ok(appdata_path) => format!("{}\\{}\\config.toml", appdata_path, BINARY_NAME),
+            Ok(appdata_path) => format!("{}\\{}\\{}", appdata_path, BINARY_NAME, file),
             Err(err) => std::panic::panic_any(err),
         },
         _ => unimplemented!(),
@@ -25,7 +25,7 @@ mod tests {
     fn test_windows_config_path() {
         match std::env::var("APPDATA") {
             Ok(appdata_path) => assert_eq!(
-                config_path(),
+                config_path("config.toml"),
                 format!("{}\\{}\\config.toml", appdata_path, BINARY_NAME)
             ),
             Err(err) => std::panic::panic_any(err),
@@ -37,7 +37,7 @@ mod tests {
     fn test_unix_config_path() {
         match std::env::var("HOME") {
             Ok(env_home_path) => assert_eq!(
-                config_path(),
+                config_path("config.toml"),
                 format!("{}/.config/{}/config.toml", env_home_path, BINARY_NAME)
             ),
             Err(err) => std::panic::panic_any(err),
