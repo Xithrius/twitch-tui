421ef00a89a552cd7cb2d41923478c54dfcd2422
Xithrius <xithrius@gmail.com>
2022-05-14 13:57:04 -0700

    Config now auto-generates on Windows.

--- src/utils/pathing.rs
+++ src/utils/pathing.rs
@@ -1,17 +1,19 @@
-use std::panic::panic_any;
-
 const BINARY_NAME: &str = env!("CARGO_BIN_NAME");
 
 pub fn config_path() -> String {
+    let partial_path = dirs::config_dir().unwrap();
+
     match std::env::consts::OS {
-        "linux" | "macos" => match std::env::var("HOME") {
-            Ok(env_home_path) => format!("{}/.config/{}/config.toml", env_home_path, BINARY_NAME),
-            Err(err) => panic_any(err),
-        },
-        "windows" => match std::env::var("APPDATA") {
-            Ok(appdata_path) => format!("{}\\{}\\config.toml", appdata_path, BINARY_NAME),
-            Err(err) => std::panic::panic_any(err),
-        },
+        "linux" | "macos" => format!(
+            "{}/{}/config.toml",
+            partial_path.to_string_lossy(),
+            BINARY_NAME
+        ),
+        "windows" => format!(
+            "{}\\{}\\config.toml",
+            partial_path.to_string_lossy(),
+            BINARY_NAME
+        ),
         _ => unimplemented!(),
     }
 }
