c3ab67112fc00498a16b86a7e72524c4baec2ef7
Xithrius <xithrius@gmail.com>
2023-05-29 23:31:08 -0700

    It is possible for channels to be 3 characters long

--- src/ui/statics.rs
+++ src/ui/statics.rs
@@ -79,6 +79,8 @@ lazy_static! {
     pub static ref TWITCH_MESSAGE_LIMIT: usize = 500;
 
     // https://www.reddit.com/r/Twitch/comments/32w5b2/username_requirements/
+    // This thread is from 8 years ago, so this regex match may be outdated.
+    // It is now possible to have channel names be 3 characters, such as "ppy".
     pub static ref NAME_MAX_CHARACTERS: usize = 25;
-    pub static ref NAME_RESTRICTION_REGEX: &'static str = "^[a-zA-Z0-9_]{4,25}$";
+    pub static ref NAME_RESTRICTION_REGEX: &'static str = "^[a-zA-Z0-9_]{3,25}$";
 }
