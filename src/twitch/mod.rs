6d3eb7c5ebd038712dd26f11f944e268cc81463a
Xithrius <xithrius@gmail.com>
2023-08-07 23:03:53 -0700

    Base for clearing the chat and deleting messages

--- src/twitch/mod.rs
+++ src/twitch/mod.rs
@@ -206,6 +206,10 @@ async fn handle_message_command(
                             .unwrap();
                     }
                 }
+                // https://dev.twitch.tv/docs/irc/tags/#clearchat-tags
+                // https://docs.rs/irc/latest/irc/client/prelude/enum.Command.html
+                // https://datatracker.ietf.org/doc/html/rfc2812#section-3.4.7
+                "CLEARCHAT" | "CLEARMSG" => {}
                 _ => (),
             }
         }
