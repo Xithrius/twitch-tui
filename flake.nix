8a6806c239e3fb155580c6ac3bad3419eecc4f1b
ves <28784043+vesdev@users.noreply.github.com>
2024-03-02 20:16:28 +0200

    Fix flake rustc version (#547)

--- flake.nix
+++ flake.nix
@@ -1,27 +1,32 @@
 {
   description = "Twitch chat in the terminal.";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.rust-overlay.url = "github:oxalica/rust-overlay";
 
-  outputs = { self, nixpkgs }:
-  let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-    manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
-  in {
-    packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
-      pname = manifest.name;
-      version = manifest.version;
+  outputs = { self, nixpkgs, rust-overlay }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      toolchain = rust-overlay.packages.${system}.rust.minimal;
+      manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+    in {
+      packages.${system}.default = (pkgs.makeRustPlatform {
+        cargo = toolchain;
+        rustc = toolchain;
+      }).buildRustPackage {
+        pname = manifest.name;
+        version = manifest.version;
 
-      buildInputs = [pkgs.openssl.dev];
-      nativeBuildInputs = [pkgs.pkg-config];
+        buildInputs = [ pkgs.openssl.dev ];
+        nativeBuildInputs = [ pkgs.pkg-config ];
 
-      src = pkgs.lib.cleanSource ./.;
-      cargoLock.lockFile = ./Cargo.lock;
+        src = pkgs.lib.cleanSource ./.;
+        cargoLock.lockFile = ./Cargo.lock;
+      };
+
+      apps.${system}.default = {
+        type = "app";
+        program = "${self.packages.${system}.default}/bin/twt";
+      };
     };
-
-    apps.${system}.default = {
-      type = "app";
-      program = "${self.packages.${system}.default}/bin/twt";
-    };
-  };
 }
