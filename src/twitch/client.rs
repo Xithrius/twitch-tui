67286c8e31d3e9ba554e5dd2f31c4913f184876a
Xithrius <xithrius@fastmail.com>
2025-04-18 20:45:36 -0700

    Removed phantom data state from twitch client

--- src/twitch/client.rs
+++ src/twitch/client.rs
@@ -6,35 +6,25 @@ use reqwest::Client;
 use super::oauth::{TwitchOauth, get_twitch_client, get_twitch_client_oauth};
 use crate::handlers::config::TwitchConfig;
 
-struct Authenticated;
-struct Unauthenticated;
-
-pub struct TwitchClient<State> {
+pub struct TwitchClient {
     client: Client,
     oauth: TwitchOauth,
     session_id: String,
-    state: PhantomData<State>,
 }
 
-impl TwitchClient<Unauthenticated> {
-    pub async fn new(
-        twitch_config: &TwitchConfig,
-        session_id: String,
-    ) -> Result<TwitchClient<Authenticated>> {
+impl TwitchClient {
+    pub async fn new(twitch_config: &TwitchConfig, session_id: String) -> Result<Self> {
         let token = twitch_config.token.clone();
 
         let oauth = get_twitch_client_oauth(token.as_ref()).await?;
         let client = get_twitch_client(&oauth, token.as_ref()).await?;
 
-        let twitch_client = TwitchClient {
+        let twitch_client = Self {
             client,
             oauth,
             session_id,
-            state: PhantomData,
         };
 
         Ok(twitch_client)
     }
 }
-
-impl TwitchClient<Authenticated> {}
