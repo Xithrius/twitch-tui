c1b6df97eda50f1e27bc12995b558cd92439eb71
kaizo <kaizoplant@proton.me>
2025-10-03 16:23:39 -0400

    omfg i need to stop pushing before checking the formatting

--- src/handlers/config.rs
+++ src/handlers/config.rs
@@ -586,10 +586,7 @@ impl ToVec<(String, String)> for FiltersConfig {
 impl ToVec<(String, String)> for FrontendConfig {
     fn to_vec(&self) -> Vec<(String, String)> {
         vec![
-            (
-                "View command".to_string(),
-                self.view_command.join(" "),
-            ),
+            ("View command".to_string(), self.view_command.join(" ")),
             (
                 "Autostart view command".to_string(),
                 self.autostart_view_command.to_string(),
