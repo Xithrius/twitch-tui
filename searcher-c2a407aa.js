6a414fd3b103ce6ca38b7da79fcab6bb92792999
Xithrius <Xithrius@users.noreply.github.com>
2026-02-16 05:12:23 +0000

    deploy: a20f17cb2755cb182f3cb461446cb551af6835ae

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-dc21b787.js',
+                path_to_root + 'searchindex-d2147c24.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
